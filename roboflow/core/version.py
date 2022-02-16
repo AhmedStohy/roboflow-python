4495bb4e88e849bd46e027961bf2ee260b8a27a4
AhmedStohy <57235472+AhmedStohy@users.noreply.github.com>
2022-02-16 04:26:35 +0200

    Update version.py

--- roboflow/core/version.py
+++ roboflow/core/version.py
@@ -82,7 +82,7 @@ class Version():
             sys.stdout.write("\r" + progress_message)
             sys.stdout.flush()
         
-        wget.download(link, out=location + "/roboflow.zip", bar=bar_progress)
+        wget.download(link, out=location + "/roboflow.zip")
         sys.stdout.write("\n")
         sys.stdout.flush()
 
